85ee48ec05b87c5e576a95d1dbae76bb12fa259d
Lev Malinovski <lev.malinovski@motherlink.io>
2024-11-23 11:45:50 +0200

    init

---
+++ src/main/java/io/github/purchase_approval_system/purchase_approval/PurchaseApprovalApplication.java
@@ -0,0 +1,13 @@
+package io.github.purchase_approval_system.purchase_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseApprovalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/purchase_approval_system/purchase_approval/PurchaseApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.purchase_approval_system.purchase_approval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseApprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
